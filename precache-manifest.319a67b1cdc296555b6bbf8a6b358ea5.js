91b7aedfd5923c4bba177dd66b3521d1e1eb51a0
Loh Eng Liang <lohengliang@gmail.com>
2020-02-07 15:43:07 +0800

    Updates

---
+++ precache-manifest.319a67b1cdc296555b6bbf8a6b358ea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dedd024a531e0e9c3dc64f96dc6a1cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf8631f3d81ddefdb690",
+    "url": "/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "764c26720541a7e9f0e6",
+    "url": "/static/css/main.abe276f2.chunk.css"
+  },
+  {
+    "revision": "bf8631f3d81ddefdb690",
+    "url": "/static/js/2.0975de93.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.0975de93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764c26720541a7e9f0e6",
+    "url": "/static/js/main.fce44fc9.chunk.js"
+  },
+  {
+    "revision": "4c05863295aa7eb5c0db",
+    "url": "/static/js/runtime-main.989f7e98.js"
+  }
+]);
